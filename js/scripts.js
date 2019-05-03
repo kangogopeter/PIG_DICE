27b44c6c9954808c3eddbce701d7ca17718dde98
kangogopeter <peterkangogo501@gmail.com>
2019-05-03 16:28:11 +0300

    new commitments

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,18 @@
+var total = 0
 
-$(document).ready(function() {
-  $(".roll").click(function player1() {
-    var kangogo = Math.floor(Math.random() * 6) + 1;
-    parseInt($(".scores").text(kangogo));
+function player1(){}
 
-    if (kangogo != 1) {
-      total += kangogo;
-      parseInt($(".peter").text(total));
-    } else {
-      total = 0;
-      parseInt($(".peter").text(total));
-    }
-  });
-});
+function pet(){}
 
-
-/*the rules in the rule button*/
 $(document).ready(function(){
-	$(".coldplay").click(function(event){
-		$(".worse").slideDown(10000);
-		$(".worse").fadeOut(10000);
-	});
-});
+	$(".btn-roll").click(function player1(){
 
+		var kangogo = Math.floor(Math.random() * 6) + 1;
+	parseInt($(".scores").text(kangogo));
 
-
-/*refresh browser*/
-$(document).ready(function(){
-$(".new").click(function() {
-setTimeout(location.reload(true), t);
-});
-});
-
-
-
-/*second player*/
-
-
-$(document).ready(function() {
-  $(".hold").click(function player2() {
-    var ludut = Math.floor(Math.random() * 6) + 1;
-    parseInt($(".scores3").text(ludut));
-
-    if (ludut != 1) {
-      total += ludut;
-      parseInt($(".finali").text(total));
-    } else {
-      total = 0;
-      parseInt($(".finali").text(total));
-    }
-  });
-});
+	if(kangogo !==1){
+		total += kangogo;
+		parseInt($("kangogo"))
+	}
+	})
+})
